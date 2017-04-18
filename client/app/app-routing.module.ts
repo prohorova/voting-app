19fc18fc653c42feace95f94a9a1f7974f9d3f5d
Ann Prokhorova <prokhorova.ann26@gmail.com>
2017-04-18 23:08:59 +0400

    remove unused client files

--- client/app/app-routing.module.ts
+++ client/app/app-routing.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent }
+
 ];
 
 @NgModule({
--- client/app/shared/shared.module.ts
+++ client/app/shared/shared.module.ts
@@ -2,26 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
-import { ToastComponent } from './toast/toast.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCheckboxModule } from '@angular/material';
 
 @NgModule({
-   imports: [
-       BrowserModule,
-       FormsModule,
-       ReactiveFormsModule,
-       HttpModule
-   ],
-   exports: [
-       // Shared Modules
-       BrowserModule,
-       FormsModule,
-       ReactiveFormsModule,
-       HttpModule,
-       // Shared Components
-       ToastComponent
-   ],
-   declarations: [ToastComponent],
-   providers: [ToastComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+  ],
+  exports: [
+    // Shared Modules
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    // Angular Material Modules
+    BrowserAnimationsModule,
+    MdButtonModule, MdCheckboxModule,
+  ]
 })
 export class SharedModule { }
