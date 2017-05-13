3002b6aa05d1017b16dc95faaf106e2c6d5fab8e
Ann Prokhorova <prokhorova.ann26@gmail.com>
2017-05-14 02:27:56 +0400

    add redirects for route guards

--- client/app/core/unauth-guard.service.ts
+++ client/app/core/unauth-guard.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable()
 export class UnauthGuardService implements CanActivate {
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService,
+              private router: Router) { }
 
   canActivate() {
-    return !this.auth.isLoggedIn();
+    if (this.auth.isLoggedIn()) {
+      this.router.navigate(['/']);
+    }
+    return true;
   }
 
 }
