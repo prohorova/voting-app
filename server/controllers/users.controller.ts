c6ab527ce8f05097619f42ce11c00a9ed0d8f158
Ann Prokhorova <prokhorova.ann26@gmail.com>
2017-05-14 23:31:13 +0400

    server auth small changes

--- server/controllers/users.controller.ts
+++ server/controllers/users.controller.ts
@@ -4,46 +4,38 @@ import User from '../models/user.model';
 export default class UsersController {
 
   login = (req, res, next) => {
-    if (!req.user) {
-      passport.authenticate('local', function(err, user, info) {
+    passport.authenticate('local', function(err, user, info) {
+      if (err) { return next(err); }
+      if (!user) { return res.status(409).send(info); }
+      req.logIn(user, function(err) {
         if (err) { return next(err); }
-        if (!user) { return res.status(409).send(info); }
-        req.logIn(user, function(err) {
-          if (err) { return next(err); }
-          return res.send({_id: user._id, name: user.name, email: user.email});
-        });
-      })(req, res, next);
-    } else {
-      res.status(403).send({message: 'Forbidden'});
-    }
+        return res.send({_id: user._id, name: user.name, email: user.email});
+      });
+    })(req, res, next);
   };
 
   register = (req, res) => {
-    if (!req.user) {
-      const name = req.body.name;
-      const email = req.body.email;
-      const password = req.body.password;
-      User.findOne({email}, (err, user) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({email}, (err, user) => {
+      if (err) return res.status(500).send(err);
+      if (user) {
+        return res.status(409).send({message: 'User with this email already exists'});
+      }
+
+      const newUser = new User({name, email});
+      newUser.setPassword(password);
+      newUser.provider = 'local';
+      newUser.save((err) => {
         if (err) return res.status(500).send(err);
-        if (user) {
-          return res.status(409).send({message: 'User with this email already exists'});
-        }
 
-        const newUser = new User({name, email});
-        newUser.setPassword(password);
-        newUser.provider = 'local';
-        newUser.save((err) => {
+        req.logIn(newUser, (err) => {
           if (err) return res.status(500).send(err);
-
-          req.logIn(newUser, (err) => {
-            if (err) return res.status(500).send(err);
-            return res.send({_id: newUser._id, name: newUser.name, email: newUser.email});
-          })
+          return res.send({_id: newUser._id, name: newUser.name, email: newUser.email});
         })
-      });
-    } else {
-      return res.status(403).send({message: 'Forbidden'});
-    }
+      })
+    });
   };
 
   logout = (req, res) => {
